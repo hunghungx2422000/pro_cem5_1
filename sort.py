92621705241963ef5182d3d550cd4e6ad197cacd
hunghungx2422000 <hunghungx2422000@gmail.com>
2022-11-05 15:09:41 +0300

    Change to QuickSort

--- sort.py
+++ sort.py
@@ -1,11 +1,20 @@
-#bubbleSort
-def bubbleSort(arr): 
-    n = len(arr) 
-    for i in range(n): 
-        for j in range(0, n-i-1): 
-              if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
-arr = [9, 1, 15, 28, 6] 
-bubbleSort(arr) 
-for i in range(len(arr)): 
-    print ("%d" %arr[i])
+#QuickSort
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+data = [9, 1, 15, 28, 6] 
+size = len(data) 
+quickSort(data, 0, size - 1) 
+for i in range(len(data)): 
+    print ("%d" %data[i])
